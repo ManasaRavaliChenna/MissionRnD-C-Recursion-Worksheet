2d4404f5910efecb7fe7888d0ae4869ef85577d6
cmravali@gmail.com <cmravali@gmail.com@missionrnd.com>
2016-04-14 15:28:31 +0530

    Submit Commit

--- src/Steps.cpp
+++ src/Steps.cpp
@@ -23,8 +23,39 @@
 */
 #include "stdafx.h"
 
-
+int calculate_steps(int steps, int noOfTwos,int noOfWays);
+int factorial(int number);
 int get_steps(int s)
 {
-	return 0;
+	if (s <= 0)
+		return 0;
+	else
+		return (calculate_steps(s, 1, 0) + 1);
+}
+int calculate_steps(int steps, int noOfTwos, int noOfWays)
+{
+	if (steps / 2 == noOfTwos)
+		if (noOfTwos * 2 == steps)
+			//+1 for arranging 2s
+			return noOfWays+1;
+		else
+			//all the arrangements except 1,2,2,2,2.. are calculated till here
+			//so we add no. of ways to arrange 1 is no.of twos+1
+		{
+			int p = (noOfWays + noOfTwos + 1);
+			return (noOfWays + noOfTwos + 1);
+		}
+
+	else
+	{
+		int rem = steps - noOfTwos;
+		return calculate_steps(steps, noOfTwos + 1, noOfWays + (factorial(rem) / (factorial(noOfTwos)*factorial(rem - noOfTwos))));
+	}
+}
+int factorial(int n)
+{
+	if (n == 0)
+		return 0;
+	else
+		return n*factorial(n - 1);
 }
